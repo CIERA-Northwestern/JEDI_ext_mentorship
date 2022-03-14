6ac71ac314efc9c849a79b751e1dce89bb719e1b
Alex Gurvich <agurvich@u.northwestern.edu>
2022-03-13 19:15:53 -0500

    find edges of a graph which cross eachother in the current pos_dict representation

--- network_analysis.py
+++ network_analysis.py
@@ -1,3 +1,4 @@
+import numpy as np
 import networkx.algorithms.community as nx_comm
 
 def get_pods(this_network):
@@ -46,3 +47,80 @@ def find_missing_edges(all_edges,sub_nodes,sub_edges):
 
     ## take set difference
     return list(expanded_edges-sub_edges)
+
+
+def find_planar_crossing_edges(graph,pos_dict):
+    ## could be useful: 
+    #https://networkx.org/documentation/stable/reference/algorithms/generated/networkx.algorithms.boundary.edge_boundary.html#networkx.algorithms.boundary.edge_boundary
+
+    crosses = []
+    edges = list(graph.edges.keys())
+    for i,edge1 in enumerate(edges):
+        for edge2 in edges[i+1:]:
+            if edge1 == edge2: continue
+            if determine_if_cross(edge1,edge2,pos_dict):
+                crosses += [edge1,edge2]
+
+    return crosses
+
+def determine_if_cross(edge1,edge2,pos_dict):
+    # y-y0 = m(x-x0) + b
+    node1a = edge1[0]
+    node1b = edge1[1]
+    x1a,y1a = pos_dict[node1a]
+    x1b,y1b = pos_dict[node1b]
+
+    node2a = edge2[0]
+    node2b = edge2[1]
+    x2a,y2a = pos_dict[node2a]
+    x2b,y2b = pos_dict[node2b]
+
+
+    xs = [x1a,x1b,x2a,x2b]
+    ys = [y1a,y1b,y2a,y2b]
+    xmin,xmax = np.min(xs),np.max(xs)
+    ymin,ymax = np.min(ys),np.max(ys)
+
+    if np.isclose(x1a,x1b) and np.isclose(x2a,x2b): 
+        ## two vertical lines that overlap
+        if np.isclose(x1a,x2b): return True
+        ## two vertical lines that do not overlap
+        else: return False
+    
+    m1 = (y1b-y1a)/(x1b-x1a)
+    m2 = (y2b-y2a)/(x2b-x2a)
+    if np.isinf(m1): m1 = 0
+    if np.isinf(m2): m2 = 0
+
+    ## parallel lines (but not vertical)
+    if np.isclose(m1,m2): return False
+
+    # y - y1a = m1(x-x1a)
+    # y - y2a = m2(x-x2a)
+
+    # y - m1x = -m1x1a + y1a
+    # y - m2x = -m2x2a + y2a
+
+    #[-m1 , 1][x] = [-m1x1a + y1a] -> [x] = ([-m1 , 1])^-1 [-m1x1a + y1a]
+    #[-m2 , 1][y] = [-m2x2a + y2a] -> [y] = ([-m2 , 1])    [-m2x2a + y2a]
+    for x1,y1 in zip([x1a,x1b],[y1a,y1b]):
+        for x2,y2 in zip([x2a,x2b],[y2a,y2b]):
+            if np.isclose(x1,x2) and np.isclose(y1,y2): return False 
+            vec = np.array([-m1*x1+y1,-m2*x2+y2]).reshape(2,1)
+
+    mat = np.array([[-m1,1],[-m2,1]])
+    try:
+        xcross,ycross = np.matmul(np.linalg.inv(mat),vec)[:,0]
+    except:
+        print(mat,vec)
+        import pdb; pdb.set_trace()
+
+    cross = True
+    for x1,y1 in zip([x1a,x2a],[y1a,y2a]):
+        for x2,y2 in zip([x1b,x2b],[y1b,y2b]):
+            xbad = (x1<xcross<x2 or x1>xcross>x2)
+            ybad = (y1<ycross<y2 or y1>ycross>y2)
+            #if np.isclose(x1,xcross) or np.isclose(x2,xcross): return False
+            #if np.isclose(y1,ycross) or np.isclose(y2,ycross): return False
+            cross = (cross and xbad)# and ybad)
+    return cross
